1c3d15d221ec5320997bb1162127ed4123fd94b9
Nechama Borisute <nechama907@gmail.com>
2024-02-27 13:39:18 -0500

    practice app

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+#Write Title
+st.title("Title")
+
+#Write Text 
+st.write("Text")
+
+#Display an Image
+st.image("content")
+
+#Text Field
+st.text_input("placeholder text","default value")
